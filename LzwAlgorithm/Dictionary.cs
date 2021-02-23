66920d6ab081845e73780cfde1b47526cd96d332
StenDarr <89377379140@mail.ru>
2021-02-24 00:02:48 +0300

    Dictionary added

---
+++ LzwAlgorithm/Dictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace LzwAlgorithm
+{
+    class Dictionary
+    {   
+        private class Node
+        {
+            public char symbol;
+            public int code;
+            public Hashtable hashtable;
+        }
+
+        private Node root;
+        private int maxCode;
+
+        public bool Contains(string str)
+        {
+            int index = 0;
+            Node currentNode = root;
+            while (index < str.Length)
+            {
+                if (!root.hashtable.Contains(str[index]))
+                {
+                    return false;
+                }
+                currentNode = (Node)root.hashtable[str[index]];
+                index++;
+            }
+            return true;
+        }
+
+        public void Add(string str)
+        {
+            int index = 0;
+            var currentNode = root;
+            while (index != str[str.Length - 1])
+            {
+                currentNode = (Node)root.hashtable[str[index]];
+                index++;
+            }
+            currentNode.hashtable.Add(str[index], new Node { symbol = str[index], code = maxCode++ });
+        }
+    }
+}

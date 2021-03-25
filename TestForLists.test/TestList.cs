45e23beaf3ced6cecd8ba2287dbeba49e1d8def7
StenDarr <89377379140@mail.ru>
2021-03-25 22:36:49 +0300

    Added list and test for list

---
+++ TestForLists.test/TestList.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+using WorkWithLists;
+
+namespace TestForLists.test
+{
+    public class Tests
+    {
+        private WorkWithLists.List list; 
+
+        [SetUp]
+        public void Setup()
+        {
+            list = new WorkWithLists.List();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+        }
+
+        [Test]
+        public void TestForAdd()
+        {
+            list.Add(4);
+            Assert.IsTrue(list.Length == 4);
+            Assert.IsTrue(list[3] == 4);
+        }
+
+        [Test]
+        public void TestForRemove()
+        {
+            list.Remove(0);
+            list.Remove(1);
+            Assert.IsTrue(list.Length == 1);
+            Assert.IsTrue(list[0] == 2);
+        }
+
+        [Test]
+        public void TestForInsert()
+        {
+            list.Insert(0, 5);
+            list.Insert(4, 11);
+            list.Insert(3, 21);
+            Assert.IsTrue(list[0] == 5);
+            Assert.IsTrue(list[3] == 21);
+            Assert.IsTrue(list[5] == 11);
+        }
+    }
+}
---
+++ WorkWithLists/List.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkWithLists
+{
+    public class List
+    {   
+        public int this[int i]
+        {
+            get => GetValue(i);
+            set => ChangeValue(i, value);
+        }
+
+        private class Node
+        {   
+            public Node(int value, Node next)
+            {
+                Value = value;
+                Next = next;
+            }
+
+            public int Value { get; set; }
+
+            public Node Next { get; set; }
+        }
+
+        public int Length { get => length; }
+
+        private int length;
+        private Node root;
+        private Node tail;
+
+        public void Add(int value)
+        {
+            length++;
+            if (root == null)
+            {
+                root = new Node(value, null);
+                tail = root;
+                return;
+            }
+            tail.Next = new Node(value, null);
+            tail = tail.Next;
+        }
+
+        public void Insert(int index, int value)
+        {
+            if (index < 0 || index > length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (index == 0)
+            {
+                var node = new Node(value, root);
+                root = node;
+                if (length == 0)
+                {
+                    tail = node;
+                }
+                length++;
+                return;
+            }
+            var currentNode = root;
+            for (int i = 0; i < index - 1; i++)
+            {
+                currentNode = currentNode.Next;
+            }
+            var newNode = new Node(value, currentNode.Next);
+            currentNode.Next = newNode;
+            if (currentNode == tail)
+            {
+                tail = newNode;
+            }
+            length++;
+        }
+
+        public void Remove(int index)
+        {
+            if (index < 0 || index >= length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (index == 0)
+            {
+                if (length == 0)
+                {
+
+                }
+                if (length == 1)
+                {
+                    root = null;
+                    tail = null;
+                    length--;
+                    return;
+                }
+                root = root.Next;
+                length--;
+                return;
+            }
+            var currentNode = root;
+            for (int i = 0; i < index - 1; i++)
+            {
+                currentNode = currentNode.Next;
+            }
+            if (currentNode.Next == tail)
+            {
+                tail = currentNode;
+                currentNode.Next = null;
+                length--;
+                return;
+            }
+            currentNode.Next = currentNode.Next.Next;
+            length--;
+        }
+
+        private Node FindNode(int index)
+        {
+            if (index < 0 || index >= length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            var currentNode = root;
+            int counter = 0;
+            while (currentNode != null)
+            {
+                if (counter == index)
+                {
+                    return currentNode;
+                }
+                currentNode = currentNode.Next;
+                counter++;
+            }
+            return null;
+        }
+        private void ChangeValue(int index, int newValue)
+        {
+            var node = FindNode(index);
+            node.Value = newValue;
+        }
+
+        private int GetValue(int index)
+        {
+            var node = FindNode(index);
+            return node.Value;
+        }
+    }
+}

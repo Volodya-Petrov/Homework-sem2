a01469a58e67b41f3d1d67f9afae9f6615cb6a67
StenDarr <89377379140@mail.ru>
2021-04-02 12:46:14 +0300

    Added everything

---
+++ Test/Task1/CoordinateBiggerLengthOfVectorException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Task1
+{
+    [Serializable]
+    public class CoordinateBiggerLengthOfVectorException : Exception
+    {
+        public CoordinateBiggerLengthOfVectorException() { }
+
+        public CoordinateBiggerLengthOfVectorException(string message) : base(message) { }
+
+        public CoordinateBiggerLengthOfVectorException(string message, Exception inner) : base(message, inner) { }
+
+        protected CoordinateBiggerLengthOfVectorException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+    }
+}
---
+++ Test/Task1/VectorsLengthsAreNotEqualException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Task1
+{
+    [Serializable]
+    public class VectorsLengthsAreNotEqualException : Exception
+    {
+        public VectorsLengthsAreNotEqualException() { }
+
+        public VectorsLengthsAreNotEqualException(string message) : base(message) { }
+
+        public VectorsLengthsAreNotEqualException(string message, Exception inner) : base(message, inner) { }
+
+        protected VectorsLengthsAreNotEqualException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+    }
+}

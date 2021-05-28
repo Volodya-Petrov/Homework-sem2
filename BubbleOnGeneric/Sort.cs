9c245ea6309358739627dd5381779dfe9236d76f
StenDarr <89377379140@mail.ru>
2021-05-28 12:44:51 +0300

    added everything

---
+++ BubbleOnGeneric/Sort.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace BubbleOnGeneric
+{
+    /// <summary>
+    /// класс сортировок
+    /// </summary>
+    public static class Sort
+    {
+        /// <summary>
+        /// сортировка пузырьком
+        /// </summary>
+        public static void BubbleSort<T>(List<T> list, Comparer<T> comparer)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count - i - 1; j++)
+                {
+                    if (comparer.Compare(list[j], list[j + 1]) >= 0)
+                    {
+                        var helper = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = helper;
+                    }
+                }
+            }
+        }
+    }
+}
